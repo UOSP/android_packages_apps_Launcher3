74bdb8b9bc111bb3935d19ea8f7459e156edc898
Anay Wadhera <awadhera@berkeley.edu>
2020-10-10 22:46:40 -0700

    UospLauncher: google feed integration
    Change-Id: I4dd635369513e7e8db8f8e4363acb017e0ce8eb1

--- src_base/com/android/launcher3/UospLauncher.java
+++ src_base/com/android/launcher3/UospLauncher.java
@@ -1,7 +1,13 @@
 package com.android.launcher3;
 
 import com.android.launcher3.Launcher;
+import com.android.systemui.plugins.shared.LauncherOverlayManager;
 
 public class UospLauncher extends Launcher {
+
+    @Override
+    protected LauncherOverlayManager getDefaultOverlay() {
+        return new OverlayCallbackImpl(this);
+    }
 
 }
